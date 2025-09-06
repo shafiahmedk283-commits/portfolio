ee96ad0a1d69e394807c2259e2980b32061324a6
shafiahmedk283-commits <shafiahmedk283@gmail.com>
2025-09-06 23:15:34 +0500

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,10 +1,12 @@
 const closeBtn = document.querySelector('.close_btn');
-const project1 = document.querySelector('.content');
+const project1 = document.querySelector('.project1_box');
 const project2 = document.querySelector('.parallax_box');
 const project3 = document.querySelector('.tic_box');
+const cer_box= document.querySelector('.cer_box');
 const Btn1 = document.querySelector('.btn1');
 const Btn2 = document.querySelector('.btn2');
 const Btn3 = document.querySelector('.btn3');
+const Btn4 = document.querySelector('.btn4');
 const p_tab = document.querySelector('.p_tab');
 
 Btn1.addEventListener('click', () => {
@@ -27,11 +29,19 @@ Btn3.addEventListener('click', () => {
   project1.style.display = 'none';
   project2.style.display = 'none';
 });
+Btn4.addEventListener('click', () => {
+  p_tab.style.display = 'flex';
+  project3.style.display='none';
+  project1.style.display = 'none';
+  project2.style.display = 'none';
+  cer_box.style.display = 'block';
+});
 
 closeBtn.addEventListener('click', () => {
   p_tab.style.display = 'none';
   project1.style.display = 'none';
   project2.style.display = 'none';
   project3.style.display = 'none';
+  cer_box.style.display = 'none';
 
 });
